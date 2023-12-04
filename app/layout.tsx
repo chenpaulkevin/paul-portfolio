481b702988cfccfebae0ee2f4321921a4e03464c
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-12-04 15:00:45 +0800

    Add SEO changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,14 @@ export const metadata: Metadata = {
   title: "Paul Chen - Portfolio",
   description:
     "Paul Chen is a Creative Developer with 3 years of experience based in Manila, Philippines.",
+  other: {
+    "theme-coloer": "#1E2124",
+    "color-scheme": "dark-only",
+    "twitter-image": "https://i.ibb.co/tsPKgBn/seo-image.jpg",
+    "og:url": "https://paulkevinchen.vercel.app/",
+    "og:image": "https://i.ibb.co/tsPKgBn/seo-image.jpg",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
